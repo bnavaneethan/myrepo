02b1e0cd2a94c6cbdaaa5e931018256e1317709b
Bharath Navaneethan <bharat_narra@yahoo.com>
2013-07-10 17:10:40 +0000

    Modified fibo.js

--- fibo.js
+++ fibo.js
@@ -6,11 +6,6 @@ var fibonacci = function(n) {
 	else if (n > 2) { return fibonacci(n - 1) + fibonacci(n - 2);}
 };
 
-var fibonacci2 = function(n) {
-	var phi = (1 + Math.sqrt(5))/2;
-	return Math.round((Math.pow(phi, n) - Math.pow(1 - phi, n))/Math.sqrt(5));
-};
-
 var firstkfib = function(k) {
 	var i = 1;
 	var arr = [];
